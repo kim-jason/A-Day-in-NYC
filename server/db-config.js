2f8b04f23f8e2463af912579ed40c42114f84eb0
Anushrut Shah <47458565+anushsh@users.noreply.github.com>
2021-01-27 00:28:09 -0500

    Delete db-config.js

--- server/db-config.js
+++
@@ -1,8 +0,0 @@
-// Private. Will not be included in submission
-module.exports = {
-  host: "cis550-group42.c3imjerhawwv.us-east-1.rds.amazonaws.com",
-  port: "3306",
-  user: "admin",
-  password: "nZ8uT5eQ6nI2aW2k",
-  database: "group42"
-};
