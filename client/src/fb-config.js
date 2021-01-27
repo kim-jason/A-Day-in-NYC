11e22307c111891ea8b3c2fa16cf159ef47bd9b4
Anushrut Shah <47458565+anushsh@users.noreply.github.com>
2021-01-27 00:28:56 -0500

    Delete fb-config.js

--- client/src/fb-config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    apiKey: "AIzaSyCqotvPiCbft4mE-o3qd16k0bCFtGi_ihc",
-    authDomain: "cis550project-3b56f.firebaseapp.com",
-    projectId: "cis550project-3b56f",
-    storageBucket: "cis550project-3b56f.appspot.com",
-    messagingSenderId: "493805368946",
-    appId: "1:493805368946:web:aae9b0158b9639b3e6b4eb",
-    measurementId: "G-D4G93BLSMW"
-  };
